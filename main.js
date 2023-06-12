966277f1880479dcc8f0fd64954eb09db8a413c8
namahee <85374690+namahee@users.noreply.github.com>
2023-06-11 23:22:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var x = false; 
+function decides() {
+  if(!x){
+    document.getElementById("audio").play();
+    document.getElementById("image").src = "./pause_image.png";
+    document.getElementById("playing").innerHTML = "Yot Club - YKWIM is playing...";
+    document.getElementById("br1").hidden = false;
+    document.getElementById("br2").hidden = false;
+    x = true;
+  } else {
+    document.getElementById("audio").pause();
+    document.getElementById("image").src = "./play_image.png"
+    document.getElementById("playing").innerHTML = "";
+    document.getElementById("br1").hidden = true;
+    document.getElementById("br2").hidden = true;
+    x = false;
+  }
+}
+
+var y = false;
+function twoSides() {
+  if (!y) {
+    document.getElementById("two").innerHTML = "ódio";
+    y = true;
+  } else {
+    document.getElementById("two").innerHTML = "love";
+    document.getElementById("two").href = "/odio.html";
+  }
+}
+
+var z = false;
+function odSides() {
+  if (!z) {
+    document.getElementById("odio").innerHTML = "voltar";
+    z = true;
+  } else {
+    document.getElementById("odio").innerHTML = "ódio";
+    document.getElementById("odio").href = "/";
+  }
+}
